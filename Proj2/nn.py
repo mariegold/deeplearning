05e1358100377cb5cca3cedc4e1d1147882a0597
Marie <marie@biolkova.net>
2021-04-18 23:46:10 +0200

    Load data, structure for Proj2

---
+++ Proj2/nn.py
@@ -0,0 +1,48 @@
+import torch
+
+torch.set_grad_enabled(False)
+
+class Module(object):
+
+    def forward(self, *input):
+        raise NotImplementedError
+
+    def backward(self, *gradwrtoutput):
+        raise NotImplementedError
+
+    def param(self):
+        return []
+
+class Linear(Module):
+
+    def __init__(self, input_dim, output_dim):
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.w = None # initialize weights
+        self.b = None # initialize bias
+
+    def forward(self, input):
+        return self.w @ inputs.t() + self.b
+
+    def backward(self, gradwrtoutput):
+        return gradwrtoutput @ self.w
+
+    def param(self):
+        return [self.w, self.b]
+
+
+class ReLu(Module):
+
+    def forward(self, input):
+        return input.clamp(min=0)
+
+    def backward(self, output, gradwrtoutput):
+        return gradwrtoutput * (output > 0)
+
+class Tanh(Module):
+
+    def forward(self, input):
+        return input.tanh()
+
+    def backward(self, output, gradwrtoutput):
+        return gradwrtoutput * (1 - output**2)
--- Proj2/test.py
+++ Proj2/test.py
@@ -1,1 +1,11 @@
+import nn
+import math
 
+def generate_dataset(n):
+    input = torch.empty(n, 2).uniform_(0, 1)
+    target = (((input - 0.5).pow(2).sum(1)) < (1 / (2*math.pi)**0.5)).long()
+    return input, target
+
+if __name__ == '__main__':
+    train_input, train_target = generate_dataset(1000)
+    test_input, test_target = generate_dataset(1000)
