3e539925611a5c9c7526f1e94fcd55597c5d60ea
MinhTri-Pham <s1612839@sms.ed.ac.uk>
2021-04-30 17:37:45 +0200

    Added basic model without auxiliary loss and similar model with auxiliary loss. Small changes to helpers

--- Proj1/helpers.py
+++ Proj1/helpers.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 from torch import optim
 
-def train_model(model, train_input, train_target, mini_batch_size = 50, nb_epochs = 25, lr = 0.1):
+def train_model(model, train_input, train_target, mini_batch_size = 50, nb_epochs = 25, lr = 0.001):
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr = lr) 
     for e in range(nb_epochs):
@@ -13,12 +13,12 @@ def train_model(model, train_input, train_target, mini_batch_size = 50, nb_epoch
             loss.backward()
             optimizer.step()
 
-def train_model_with_aux_loss(model, train_input, train_target, train_classes, mini_batch_size = 50, nb_epochs = 25, lr = 0.1):
+def train_model_with_aux_loss(model, train_input, train_target, train_classes, mini_batch_size = 50, nb_epochs = 25, lr = 0.001):
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr = lr) 
     for e in range(nb_epochs):
         for b in range(0, train_input.size(0), mini_batch_size): 
-            output_bool, output_digit1, output_digit2 = model(train_target.narrow(0, b, mini_batch_size))
+            output_bool, output_digit1, output_digit2 = model(train_input.narrow(0, b, mini_batch_size))
             loss_bool = criterion(output_bool, train_target.narrow(0, b, mini_batch_size))
             loss_digit1 = criterion(output_digit1, train_classes.narrow(0, b, mini_batch_size)[:,0])
             loss_digit2 = criterion(output_digit2, train_classes.narrow(0, b, mini_batch_size)[:,1])
--- Proj1/test.py
+++ Proj1/test.py
@@ -1,1 +1,23 @@
+import dlc_practical_prologue as prologue
+from helpers import *
+from models import *
 
+if __name__ == '__main__':
+  n_pairs = 1000
+  train_input, train_target, train_classes, test_input, test_target, test_classes = prologue.generate_pair_sets(n_pairs)
+
+  model = BaseNet()
+  train_model(model, train_input, train_target)
+  nb_errors_test = compute_nb_errors(model, test_input, test_target)
+  nb_errors_train = compute_nb_errors(model, train_input, train_target)
+
+  print("Test accuracy: {} " .format(1 - nb_errors_test/n_pairs))
+  print("Train accuracy: {} " .format(1 - nb_errors_train/n_pairs))
+
+  model_aux = BaseNetAux()
+  train_model_with_aux_loss(model_aux, train_input, train_target, train_classes)
+  nb_errors_test_aux = compute_nb_errors_with_aux_loss(model_aux, test_input, test_target)
+  nb_errors_train_aux = compute_nb_errors_with_aux_loss(model_aux, train_input, train_target)
+  
+  print("Test accuracy with aux. loss: {} " .format(1 - nb_errors_test_aux/n_pairs))
+  print("Train accuracy with aux. loss: {} " .format(1 - nb_errors_train_aux/n_pairs))
