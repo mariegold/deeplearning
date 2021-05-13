a6d4b44eb1d648455892b0b41f4ab0abfb3a390c
MinhTri-Pham <s1612839@sms.ed.ac.uk>
2021-05-13 22:04:39 +0200

    Small improvement

--- Proj1/test.py
+++ Proj1/test.py
@@ -15,15 +15,19 @@ if __name__ == '__main__':
   model_base_mean, model_base_std, model_aux_mean, model_aux_std, model_ws_mean, model_ws_std, model_ws_aux_mean, model_ws_aux_std = performance_estimation(datasets)
 
   # Find hyperparameter setting for each model that achieves the best mean accuracy
-  # Print the mean accuracy and the corresponding std
+  # Print the hyperparameter values, mean accuracy and the corresponding std
   best_base_params = max(model_base_mean.items(), key = lambda k : k[1])
-  print('Best accuracy with BaseNet: {:.3f} +/- {}.'.format(model_base_mean[best_base_params], model_base_std[best_base_params])) 
+  print('Best (lr, mini_batch_size, use_bn, dropout rate) combination with BaseNet:', best_base_params) 
+  print('Best accuracy with BaseNet: {:.3f} +/- {:.3f}.'.format(model_base_mean[best_base_params], model_base_std[best_base_params])) 
 
   best_aux_params = max(model_aux_mean.items(), key = lambda k : k[1])
-  print('Best accuracy with BaseNetAux: {:.3f} +/- {}.'.format(model_aux_mean[best_aux_params], model_aux_std[best_aux_params])) 
+  print('Best (lr, mini_batch_size, use_bn, dropout rate) combination with BaseNetAux:', best_aux_params) 
+  print('Best accuracy with BaseNetAux: {:.3f} +/- {:.3f}.'.format(model_aux_mean[best_aux_params], model_aux_std[best_aux_params])) 
 
   best_ws_params = max(model_ws_mean.items(), key = lambda k : k[1])
-  print('Best accuracy with BaseNetWeightShare: {:.3f} +/- {}.'.format(model_ws_mean[best_ws_params], model_base_std[best_ws_params])) 
+  print('Best (lr, mini_batch_size, use_bn, dropout rate) combination with BaseNetWeightShare:', best_ws_params) 
+  print('Best accuracy with BaseNetWeightShare: {:.3f} +/- {:.3f}.'.format(model_ws_mean[best_ws_params], model_base_std[best_ws_params])) 
 
   best_ws_aux_params = max(model_ws_aux_mean.items(), key = lambda k : k[1])
-  print('Best accuracy with BaseNetWeightShareAux: {:.3f} +/- {}.'.format(model_ws_aux_mean[best_ws_aux_params], model_ws_aux_std[best_ws_aux_params])) 
+  print('Best (lr, mini_batch_size, use_bn, dropout rate) combination with BaseNetWeightShareAux:', best_ws_aux_params) 
+  print('Best accuracy with BaseNetWeightShareAux: {:.3f} +/- {:.3f}.'.format(model_ws_aux_mean[best_ws_aux_params], model_ws_aux_std[best_ws_aux_params])) 
